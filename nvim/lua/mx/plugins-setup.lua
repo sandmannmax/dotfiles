11a3a0d6d5ecce1f3f00db1696bc1ad8dbf92cc3
sandmannmax <sandmannmaxxer@gmail.com>
2023-05-19 11:32:34 +0200

    changed Alacritty window and added vimtext

--- nvim/lua/mx/plugins-setup.lua
+++ nvim/lua/mx/plugins-setup.lua
@@ -1,13 +1,13 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
@@ -22,85 +22,88 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
 return packer.startup(function(use)
-  use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim")
 
-  -- lua functions that many plugins use
-  use("nvim-lua/plenary.nvim")
-  
-  use("bluz71/vim-nightfly-guicolors")
+	-- lua functions that many plugins use
+	use("nvim-lua/plenary.nvim")
 
-  -- tmux & split window navigation
-  use("christoomey/vim-tmux-navigator")
+	use("bluz71/vim-nightfly-guicolors")
 
-  -- comment with gc
-  use("numToStr/Comment.nvim")
-  
-  -- file explorer
-  use("nvim-tree/nvim-tree.lua")
+	-- tmux & split window navigation
+	use("christoomey/vim-tmux-navigator")
 
-  -- icons
-  use("kyazdani42/nvim-web-devicons")
+	-- comment with gc
+	use("numToStr/Comment.nvim")
 
-  -- statusline
-  use("nvim-lualine/lualine.nvim")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
 
-  -- fuzzy finding
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+	-- icons
+	use("kyazdani42/nvim-web-devicons")
 
-  -- autocompletion
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
 
-  -- snippets
-  use("L3MON4D3/LuaSnip")
-  use("saadparwaiz1/cmp_luasnip")
-  use("rafamadriz/friendly-snippets")
+	-- fuzzy finding
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
-  -- managing & installing lsp servers
-  use("williamboman/mason.nvim")
-  use("williamboman/mason-lspconfig.nvim")
+	-- autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
 
-  -- configuring lsp servers
-  use("neovim/nvim-lspconfig")
-  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-  use({
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    requires = {
-      { "nvim-tree/nvim-web-devicons" },
-      { "nvim-treesitter/nvim-treesitter" },
-    },
-  }) -- enhanced lsp uis
-  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
 
-  -- formatting & linting
-  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	-- managing & installing lsp servers
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
 
-  -- treesitter configuration
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-      ts_update()
-    end,
-  })
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		requires = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	}) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-  -- auto closing
-  use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-  -- git signs plugin
-  use("lewis6991/gitsigns.nvim")
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+	-- git signs plugin
+	use("lewis6991/gitsigns.nvim")
+
+	-- LaTeX plugin
+	use("lervag/vimtex")
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
--- nvim/lua/mx/plugins/nvim-tree.lua
+++ nvim/lua/mx/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
 
 -- recommended settings from nvim-tree docs
@@ -8,14 +8,17 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 nvimtree.setup({
-  renderer = {
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = "→",
-          arrow_open = "↓",
-        },
-      },
-    },
-  },
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "→",
+					arrow_open = "↓",
+				},
+			},
+		},
+	},
+	filters = {
+		custom = { ".aux", ".bbl", ".fdb_latexmk", ".fls", ".lof", ".lol", ".lot", ".synctex.gz" },
+	},
 })
